275776a6cec7bdfb071c76529584e4f5d1fb6bb5
Daniel Henrique Domingues Nogueira <daniel@RM.GROUP>
2026-01-20 14:28:09 -0300

    REFACTOR : Iniciando as alterações de Refinamento

--- src/main/resources/static/js/agenda.js
+++ src/main/resources/static/js/agenda.js
@@ -66,18 +66,3 @@ function verDetalhesDia(dia, mes, ano) {
     });
 }
 
-// Função do Sidebar
-// (Também precisa ser global pois é chamada no onclick do menu)
-function toggleMenu(menuId, iconId) { 
-    var menu = document.getElementById(menuId);
-    var icon = document.getElementById(iconId);
-    if (menu) {
-        if (menu.classList.contains('d-none')) {
-            menu.classList.remove('d-none');
-            if(icon) { icon.classList.remove('fa-chevron-down'); icon.classList.add('fa-chevron-up'); }
-        } else {
-            menu.classList.add('d-none');
-            if(icon) { icon.classList.remove('fa-chevron-up'); icon.classList.add('fa-chevron-down'); }
-        }
-    }
-}
